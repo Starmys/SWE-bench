de6fbac124a123531714a175c7e52b50df65eb30
yuzhe_zhang <731054221@qq.com>
2024-09-03 02:06:33 +0000

    update main sh

--- inference_llama.sh
+++ inference_llama.sh
@@ -8,16 +8,16 @@ MODEL_NAME_OR_PATH=/mnt/teamdrive/model/DeepSeek-Coder-V2-Lite-Instruct
 # MODEL_NAME_OR_PATH=/home/chengzhang/models/Meta-Llama-3.1-8B
 # MODEL_NAME_OR_PATH=princeton-nlp/SWE-Llama-13b
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
-# for i in {4..10}
-# do
-#     python -m swebench.inference.run_llama \
-#     --dataset_path princeton-nlp/SWE-bench_oracle \
-#     --model_name_or_path $MODEL_NAME_OR_PATH \
-#     --output_dir ./outputs_long/$i \
-#     --top_p 0.8 \
-#     --split test \
-#     --temperature 0.8
-# done
+for i in {7..10}
+do
+    python -m swebench.inference.run_llama \
+    --dataset_path princeton-nlp/SWE-bench_oracle \
+    --model_name_or_path $MODEL_NAME_OR_PATH \
+    --output_dir ./outputs_long/$i \
+    --top_p 0.8 \
+    --split test \
+    --temperature 0.8
+done
 # python -m swebench.inference.run_llama \
 #     --dataset_path princeton-nlp/SWE-bench_oracle \
 #     --model_name_or_path $MODEL_NAME_OR_PATH \
@@ -26,14 +26,14 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn
 #     --temperature 0
 #  --dataset_path princeton-nlp/SWE-bench_oracle \
 # --dataset_path princeton-nlp/SWE-bench_Verified \
-for i in {1..10}
-do
-python -m swebench.harness.run_evaluation \
-    --dataset_name princeton-nlp/SWE-bench_Verified \
-    --split test \
-    --predictions_path /mnt/yuzhe/SWE-bench/outputs_long/$i/princeton-nlp__SWE-bench_oracle__test__DeepSeek-Coder-V2-Lite-Instruct__temp-0.8__top-p-0.8.jsonl \
-    --max_workers 1 \
-    --instance_ids test_all \
-    --run_id $i \
-    --clean False
-done
+# for i in {4..10}
+# do
+# python -m swebench.harness.run_evaluation \
+#     --dataset_name princeton-nlp/SWE-bench_Verified \
+#     --split test \
+#     --predictions_path /mnt/yuzhe/SWE-bench/outputs_long/$i/princeton-nlp__SWE-bench_oracle__test__DeepSeek-Coder-V2-Lite-Instruct__temp-0.8__top-p-0.8.jsonl \
+#     --max_workers 1 \
+#     --instance_ids test_all \
+#     --run_id $i \
+#     --clean False
+# done
